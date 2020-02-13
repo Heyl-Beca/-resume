c88d795d95dc4847fc3bb2a2c7fbe9803a028d20
HYL <Heyl.Beca@outlook.com>
2020-02-13 21:35:30 +0800

    添加全局样式引用

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,14 @@
 </template>
 
 <style lang="less">
-@import 'assets/css/reset'
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import 'assets/css/reset';
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 
 #nav {
   padding: 30px;
