342c40b047308fafa1146995bd3c9fe2d6d5644b
HYL <Heyl.Beca@outlook.com>
2020-02-13 22:00:18 +0800

    引入第三方插件库

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// import globalComponent from './components/global/index'
+// 引入第三方UI框架
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
